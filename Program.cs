24ce3ec1d96dd4a709453f81f71fc0f85dcebbdc
ArgEric8 <55736566+ArgEric8@users.noreply.github.com>
2019-12-12 14:52:26 +0100

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -20,55 +20,94 @@ namespace Labb2
                 // Första valet
                 if (userInput == 1)
                 {
-                    Console.WriteLine("\nGångertabellen:");
-                    for (int y = 1; y < 11; y++)
-                    {
-                        Console.WriteLine("\n");
-                        for (int x = 1; x < 11; x++)
-                        {
-                            Console.Write("{0,5}", x * y);
-                        }
-                    }
+                    firstChoice();
                 }
 
                 // Andra valet
                 else if (userInput == 2)
                 {
-                    Console.WriteLine("\nAnge fem heltal");
-                    int[] femTal = new int[5];
-                    int sum = 0;
-
-                    for (int i = 0; i < femTal.Length; i++)
-                    {
-                        femTal[i] = int.Parse(Console.ReadLine());
-                        sum += femTal[i];
-                    }
-
-                    int högstVärde = femTal[0];
-                    int lägstVärde = femTal[0];
-                    for (int j = 0; j < femTal.Length; j++)
-                        {
-                            if (högstVärde < femTal[j])
-                            {
-                                högstVärde = femTal[j];
-                            }
-
-                            if (lägstVärde > femTal[j])
-                            {
-                                lägstVärde = femTal[j];
-                            }
-                        }
-
-                    int medelVärde = sum / femTal.Length;
-                    Console.WriteLine("\nSumman är {0}.", sum);
-                    Console.WriteLine("Medelvärdet är {0}.", medelVärde);
-                    Console.WriteLine("Högsta värdet är {0} och lägsta värdet är {1}.", högstVärde, lägstVärde);
+                    secondChoice();
                 }
 
                 // Tredje valet
                 else if (userInput == 3)
                 {
-                    int[] randomTal = new int[10];
+                    thirdChoice();
+                }
+
+                // Fjärde valet
+                else if (userInput == 4)
+                {
+                    Console.WriteLine("\nAvslutar...");
+                    break;
+                }
+
+                else
+                {
+                    Console.WriteLine("Error, börja om!");
+                }
+            }
+        }
+
+        static void firstChoice ()
+        {
+            Console.WriteLine("\nGångertabellen:");
+            for (int y = 1; y < 11; y++)
+            {
+                Console.WriteLine("\n");
+                for (int x = 1; x < 11; x++)
+                {
+                    Console.Write("{0,5}", x * y);
+                }
+            }
+        
+        }
+
+        static void secondChoice ()
+        {
+            Console.WriteLine("\nAnge fem tal");
+                int[] femTal = new int[5];
+                double sum = 0;
+
+                for (int i = 0; i < femTal.Length; i++)
+                {
+                    
+                    femTal[i] = Console.ReadLine();
+                    if (double.TryParse(femTal[i], out double result))
+                        {
+                            double.Parse(femTal[i]);
+                            sum += femTal[i];
+                        }
+                    else
+                    {
+                        i--;
+                    }
+                }
+
+                int högstVärde = femTal[0];
+                int lägstVärde = femTal[0];
+                for (int j = 0; j < femTal.Length; j++)
+                    {
+                        if (högstVärde < femTal[j])
+                        {
+                            högstVärde = femTal[j];
+                        }
+
+                        if (lägstVärde > femTal[j])
+                        {
+                            lägstVärde = femTal[j];
+                        }
+                    }
+
+                int medelVärde = sum / femTal.Length;
+                Console.WriteLine("\nSumman är {0}.", sum);
+                Console.WriteLine("Medelvärdet är {0}.", medelVärde);
+                Console.WriteLine("Högsta värdet är {0} och lägsta värdet är {1}.", högstVärde, lägstVärde);
+        }
+
+        static void thirdChoice ()
+        {
+            int[] randomTal = new int[10];
                     Random randomSiffra = new Random();
 
                     Console.WriteLine("Dina tio slumpmässiga tal är följande");
@@ -83,19 +122,6 @@ namespace Labb2
                     Array.Reverse(randomTal);
                     Console.WriteLine("{0}", randomTal[i]);
                     }
-                }
-
-                // Fjärde valet
-                else if (userInput == 4)
-                {
-                    Console.WriteLine("\nAvslutar...");
-                }
-
-                else
-                {
-                    Console.WriteLine("Error, börja om!");
-                }
-            }
         }
     }
 }
